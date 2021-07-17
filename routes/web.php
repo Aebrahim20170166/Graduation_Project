6d120417e9b0c1f8d4074fbbd7dddbd427513626
Alaa Ebrahim <aebrahim20170166@gmail.com>
2021-07-17 18:53:26 +0200

    some modification

--- routes/web.php
+++ routes/web.php
@@ -127,6 +127,7 @@ Route::get('Data','Naeve\NaeveController@naeve');
 
 //Announcement
 
+Route::get('getreport','Reports\TopFive@getTopFive')->name('getreports');
 Route::view('Announcements','staff.makeAnnouncement')->name('announcements');
 Route::post('makepost','Announcement\AnnouncementController@makepost')->name('makepost');
 Route::get('updatepost','Announcement\AnnouncementController@updatepost')->name('updatepost');
